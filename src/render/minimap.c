926e6e23746cf51620d83d0e3f6de29688945c97
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2025-11-14 15:43:02 +0000

    Added some changes in draw_sq function

--- src/render/minimap.c
+++ src/render/minimap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minimap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
+/*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 17:11:21 by htrindad          #+#    #+#             */
-/*   Updated: 2025/11/13 17:55:23 by htrindad         ###   ########.fr       */
+/*   Updated: 2025/11/14 15:41:13 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 static inline void	draw_sq(size_t y, size_t x, t_rgb white, t_img *img)
 {
-	const size_t	ly = y * (MAP_S << 1);
-	const size_t	lx = x * (MAP_S << 1);
+	const size_t	ly = y * MAP_S + MAP_S;
+	const size_t	lx = x * MAP_S + MAP_S;
 	size_t		py;
 	size_t		px;
 
-	py = y * MAP_S + 1;
-	px = x * MAP_S + 1;
-	while (px < lx - 1)
+	py = y * MAP_S;
+	px = x * MAP_S;
+	while (px < lx)
 	{
-		while (py < ly - 1)
+		while (py < ly)
 			set_color(img, py++, px, white);
 		px++;
 	}
