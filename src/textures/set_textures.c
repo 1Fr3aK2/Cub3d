69a976170620c443934a0e6bd7fcdbc1be9b566e
Rafael Matos <rafael.joao.amorim.matos@gmail.com>
2025-12-11 00:33:23 +0000

    add

--- src/textures/set_textures.c
+++ src/textures/set_textures.c
@@ -6,7 +6,7 @@
 /*   By: rafael <rafael@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 02:32:49 by rafael            #+#    #+#             */
-/*   Updated: 2025/12/10 19:55:22 by rafael           ###   ########.fr       */
+/*   Updated: 2025/12/11 00:32:56 by rafael           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,20 +43,19 @@ static bool	load_texture(t_img *img, char *path)
 		return (false);
 	return (true);
 }
-bool	set_assets_texture(t_assets *assets, char **path, char **floor, char **ceiling)
+
+bool	set_assets_texture(t_assets *assets, char **path, char **floor,
+		char **ceiling)
 {
 	if (!assets || !path || !*path)
 		return (false);
 	if (!assets->textures[N].path && ft_strncmp(path[0], "NO", 3) == 0)
 		return (load_texture(&assets->textures[N], path[1]));
-	else if (!assets->textures[S].path && ft_strncmp(path[0], "SO",
-			3) == 0)
+	else if (!assets->textures[S].path && ft_strncmp(path[0], "SO", 3) == 0)
 		return (load_texture(&assets->textures[S], path[1]));
-	else if (!assets->textures[WE].path && ft_strncmp(path[0], "WE",
-			3) == 0)
+	else if (!assets->textures[WE].path && ft_strncmp(path[0], "WE", 3) == 0)
 		return (load_texture(&assets->textures[WE], path[1]));
-	else if (!assets->textures[EA].path && ft_strncmp(path[0], "EA",
-			3) == 0)
+	else if (!assets->textures[EA].path && ft_strncmp(path[0], "EA", 3) == 0)
 		return (load_texture(&assets->textures[EA], path[1]));
 	else if (!*floor && ft_strncmp(path[0], "F", 2) == 0)
 	{
@@ -74,6 +73,7 @@ bool	set_assets_texture(t_assets *assets, char **path, char **floor, char **ceil
 	}
 	return (false);
 }
+
 bool	set_texture(char *line, char **floor, char **ceiling, t_map *map)
 {
 	char	**split;
