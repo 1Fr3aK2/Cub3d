af73c4b9e3f3358401c32c8f766a03e095cd3aa5
Rafael <rafael.joao.amorim.matos@gmail.com>
2025-12-18 15:02:31 +0000

    2025-12-18

--- src/parsing/file/check_file.c
+++ src/parsing/file/check_file.c
@@ -6,7 +6,7 @@
 /*   By: raamorim <raamorim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 16:12:02 by raamorim          #+#    #+#             */
-/*   Updated: 2025/11/11 13:36:31 by raamorim         ###   ########.fr       */
+/*   Updated: 2025/12/18 15:02:06 by raamorim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,13 @@ void	check_map_name(char *file_name)
 	int		len;
 
 	if (!file_name)
-		exit_error(NULL, "ERROR:\nCHECK_MAP : Invalid pointer to file name");
+		exit_error(data_s(),
+			"ERROR:\nCHECK_MAP : Invalid pointer to file name");
 	file.fd = open(file_name, O_RDONLY);
 	if (file.fd < 0)
-		exit_error(NULL, "ERROR:\nCHECK_MAP : Error opening the file");
+		exit_error(data_s(), "ERROR:\nCHECK_MAP : Error opening the file");
 	len = ft_strlen(file_name);
 	if (len < 4 || ft_strncmp(&file_name[len - 4], ".cub", 4) != 0)
-		exit_error(NULL, "ERROR:\n: Invalid map name, should be .cub");
+		exit_error(data_s(), "ERROR:\n: Invalid map name, should be .cub");
 	close(file.fd);
 }
